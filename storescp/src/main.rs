47928e9f30b9526b75e0477e725782aba17af7dd
starsky <daihanzhang@gmail.com>
2025-08-08 13:26:50 +0800

    添加DICOM CStoreSCP To Receive DICOM File!

---
+++ storescp/src/main.rs
@@ -0,0 +1,222 @@
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    path::PathBuf,
+};
+
+use clap::Parser;
+use dicom_core::{dicom_value, DataElement, VR};
+use dicom_dictionary_std::tags;
+use dicom_object::{InMemDicomObject, StandardDataDictionary};
+use snafu::Report;
+use tracing::{error, info, Level};
+
+mod store_async;
+mod store_sync;
+mod transfer;
+use store_async::run_store_async;
+use store_sync::run_store_sync;
+
+/// DICOM C-STORE SCP
+#[derive(Debug, Parser)]
+#[command(version)]
+struct App {
+    /// Verbose mode
+    #[arg(short = 'v', long = "verbose")]
+    verbose: bool,
+    /// Calling Application Entity title
+    #[arg(long = "calling-ae-title", default_value = "STORE-SCP")]
+    calling_ae_title: String,
+    /// Enforce max pdu length
+    #[arg(short = 's', long = "strict")]
+    strict: bool,
+    /// Only accept native/uncompressed transfer syntaxes
+    #[arg(long)]
+    uncompressed_only: bool,
+    /// Accept unknown SOP classes
+    #[arg(long)]
+    promiscuous: bool,
+    /// Maximum PDU length
+    #[arg(
+        short = 'm',
+        long = "max-pdu-length",
+        default_value = "16384",
+        value_parser(clap::value_parser!(u32).range(4096..=131_072))
+    )]
+    max_pdu_length: u32,
+    /// Output directory for incoming objects
+    #[arg(short = 'o', default_value = ".")]
+    out_dir: PathBuf,
+    /// Which port to listen on
+    #[arg(short, default_value = "11111")]
+    port: u16,
+    /// Run in non-blocking mode (spins up an async task to handle each incoming stream)
+    #[arg(short, long)]
+    non_blocking: bool,
+}
+
+fn create_cstore_response(
+    message_id: u16,
+    sop_class_uid: &str,
+    sop_instance_uid: &str,
+) -> InMemDicomObject<StandardDataDictionary> {
+    InMemDicomObject::command_from_element_iter([
+        DataElement::new(
+            tags::AFFECTED_SOP_CLASS_UID,
+            VR::UI,
+            dicom_value!(Str, sop_class_uid),
+        ),
+        DataElement::new(tags::COMMAND_FIELD, VR::US, dicom_value!(U16, [0x8001])),
+        DataElement::new(
+            tags::MESSAGE_ID_BEING_RESPONDED_TO,
+            VR::US,
+            dicom_value!(U16, [message_id]),
+        ),
+        DataElement::new(
+            tags::COMMAND_DATA_SET_TYPE,
+            VR::US,
+            dicom_value!(U16, [0x0101]),
+        ),
+        DataElement::new(tags::STATUS, VR::US, dicom_value!(U16, [0x0000])),
+        DataElement::new(
+            tags::AFFECTED_SOP_INSTANCE_UID,
+            VR::UI,
+            dicom_value!(Str, sop_instance_uid),
+        ),
+    ])
+}
+
+fn create_cecho_response(message_id: u16) -> InMemDicomObject<StandardDataDictionary> {
+    InMemDicomObject::command_from_element_iter([
+        DataElement::new(tags::COMMAND_FIELD, VR::US, dicom_value!(U16, [0x8030])),
+        DataElement::new(
+            tags::MESSAGE_ID_BEING_RESPONDED_TO,
+            VR::US,
+            dicom_value!(U16, [message_id]),
+        ),
+        DataElement::new(
+            tags::COMMAND_DATA_SET_TYPE,
+            VR::US,
+            dicom_value!(U16, [0x0101]),
+        ),
+        DataElement::new(tags::STATUS, VR::US, dicom_value!(U16, [0x0000])),
+    ])
+}
+
+fn main() {
+    let app = App::parse();
+    if app.non_blocking {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async move {
+                run_async(app).await.unwrap_or_else(|e| {
+                    error!("{:?}", e);
+                    std::process::exit(-2);
+                });
+            });
+    } else {
+        run_sync(app).unwrap_or_else(|e| {
+            error!("{:?}", e);
+            std::process::exit(-2);
+        });
+    }
+}
+
+async fn run_async(args: App) -> Result<(), Box<dyn std::error::Error>> {
+    use std::sync::Arc;
+    let args = Arc::new(args);
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(if args.verbose {
+                Level::DEBUG
+            } else {
+                Level::INFO
+            })
+            .finish(),
+    )
+    .unwrap_or_else(|e| {
+        eprintln!(
+            "Could not set up global logger: {}",
+            snafu::Report::from_error(e)
+        );
+    });
+
+    std::fs::create_dir_all(&args.out_dir).unwrap_or_else(|e| {
+        error!("Could not create output directory: {}", e);
+        std::process::exit(-2);
+    });
+
+    let listen_addr = SocketAddrV4::new(Ipv4Addr::from(0), args.port);
+    let listener = tokio::net::TcpListener::bind(listen_addr).await?;
+    info!(
+        "{} listening on: tcp://{}",
+        &args.calling_ae_title, listen_addr
+    );
+
+    loop {
+        let (socket, _addr) = listener.accept().await?;
+        let args = args.clone();
+        tokio::task::spawn(async move {
+            if let Err(e) = run_store_async(socket, &args).await {
+                error!("{}", Report::from_error(e));
+            }
+        });
+    }
+}
+
+fn run_sync(args: App) -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(if args.verbose {
+                Level::DEBUG
+            } else {
+                Level::INFO
+            })
+            .finish(),
+    )
+    .unwrap_or_else(|e| {
+        eprintln!(
+            "Could not set up global logger: {}",
+            snafu::Report::from_error(e)
+        );
+    });
+
+    std::fs::create_dir_all(&args.out_dir).unwrap_or_else(|e| {
+        error!("Could not create output directory: {}", e);
+        std::process::exit(-2);
+    });
+
+    let listen_addr = SocketAddrV4::new(Ipv4Addr::from(0), args.port);
+    let listener = std::net::TcpListener::bind(listen_addr)?;
+    info!(
+        "{} listening on: tcp://{}",
+        &args.calling_ae_title, listen_addr
+    );
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(scu_stream) => {
+                if let Err(e) = run_store_sync(scu_stream, &args) {
+                    error!("{}", snafu::Report::from_error(e));
+                }
+            }
+            Err(e) => {
+                error!("{}", snafu::Report::from_error(e));
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::App;
+    use clap::CommandFactory;
+
+    #[test]
+    fn verify_cli() {
+        App::command().debug_assert();
+    }
+}
